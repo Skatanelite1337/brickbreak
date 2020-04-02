e9cc2d74c17513aa1f9192df6f6ffbb542a9b545
skatanelite1337 <antonn.jon@gmail.com>
2020-04-02 11:12:45 +0200

    massa fel

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,6 +1,63 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
-public class Gameplay extends JPanel {
+public abstract class Gameplay extends JPanel implements KeyListener, ActionListener {
+    private boolean play = false;
+    private int score = 0;
+    private int totalBricks = 21;
+    private Timer time;
+    private int delay = 8;
+    private int playerX = 310;
+    private int ballposX = 120;
+    private int ballposY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+
+    public Gameplay() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        time = new Timer(delay, this);
+        Timer.start();
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.black);
+        g.fillRect(1,1,692,592);
+        g.setColor(Color.gray);
+        g.fillRect(0,0,3,59);
+        g.fillRect(0,0,692,3);
+        g.fillRect(691,0,3,592);
+        g.setColor(Color.magenta);
+        g.fillRect(playerX,550,100,8);
+        g.setColor(Color.WHITE);
+        g.fillRect(ballposX,ballposY,20,20);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 
 
 }
--- src/Graphics.java
+++ src/Graphics.java
@@ -1,9 +1,10 @@
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class Graphics {
     public static void main(String[] args) {
         JFrame obj = new JFrame();
-        Gameplay gameplay = new Gameplay();
+        Gameplay gameplay = new gameplay();
         obj.setBounds(10,10,700,600);
         obj.setTitle("Sten spel");
         obj.setResizable(false);
@@ -14,4 +15,9 @@ public class Graphics {
 
     }
 
+    public void fillRect(int i, int i1, int i2, int i3) {
+    }
+
+    public void setColor(Color black) {
+    }
 }
